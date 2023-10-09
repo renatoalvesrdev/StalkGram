bcd5ce2b8b19387fc8ef57b1274dbf6971355c10
Renato Alves <renatoalvesribeiro0@gmail.com>
2023-10-09 20:39:26 -0300

    Creation of middleware, user validation (login and registration)

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -14,9 +14,76 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async(req, res) => {
-    res.send("Registro")
+    
+    const {name, email, password} = req.body
+
+    //check if user exists
+    const user = await User.findOne({email})
+
+    if(user) {
+        res.status(422).json({errors: ["Por favor, utilize outro e-mail."]})
+        return
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    // if user was created succesfully, return the token
+
+    if(!newUser) {
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+        return
+    }
+
+        res.status(201).json({
+            _id: newUser._id,
+            token: generateToken(newUser._id),
+        });
+}
+
+//sign user in
+const login = async (req, res) => {
+    
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+
+    //check if user exists
+    if(!user) {
+        res.status(400).json({errors: [ "Usuário não encontrado."]})
+        return
+    }
+
+    // check if password matches
+    if(!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({erros: ["Senha inválida."]})
+        return
+    }
+
+    //return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id),
+    });
+};
+
+//get current logged in user
+const getCurrentUser = async(req, res) => {
+    const user = req.user
+    
+    res.status(200).json(user);
 }
 
 module.exports = {
     register,
+    login,
+    getCurrentUser,
 }
