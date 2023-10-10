41240b27bc09d13fce76d6b19b80fb034833580e
Renato Alves <renatoalvesribeiro0@gmail.com>
2023-10-10 15:19:04 -0300

    Image upload and user update middleware + search for user in the db by id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,6 +3,8 @@ const User = require("../models/User")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
+const mongoose = require("mongoose")
+
 const jwtSecret = process.env.JWT_SECRET
 
 // Generate user token
@@ -82,8 +84,71 @@ const getCurrentUser = async(req, res) => {
     res.status(200).json(user);
 }
 
+// Update an user
+const update = async(req, res) => { 
+    const {name, password, bio} = req.body
+
+    let profileImage = null
+
+    if(req.file) {
+        profileImage = req.file.filename
+    }
+
+    const reqUser = req.user
+
+    const user = await User.findById(reqUser._id).select("-password")
+    
+    if(name) {
+        user.name = name
+    }
+
+    if(password) {
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        user.password = passwordHash
+    }
+
+    if(profileImage) {
+        user.profileImage = profileImage
+    }
+
+    if(bio) {
+        user.bio = bio
+    }
+
+    await user.save()
+
+    res.status(200).json(user)
+}
+
+// get user by id
+const getUserById = async(req, res) => {
+
+    const {id} = req.params
+
+    try {
+        const user = await User.findById(id).select("-password");
+
+        //check if user exists
+        if(!user) {
+            res.status(404).json({errors: ["Usuário não encontrado."]})
+            return
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({errors: ["Usuário não encontrado."]})
+        return
+    }
+
+    
+}
+
 module.exports = {
     register,
     login,
     getCurrentUser,
+    update,
+    getUserById
 }
