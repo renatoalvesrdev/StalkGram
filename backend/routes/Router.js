5c282575a55c4bc89834a0553f18d463fd65ca15
Renato Alves <renatoalvesribeiro0@gmail.com>
2023-10-16 19:38:39 -0300

    Undoing the organization of routes due to bugs

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,7 +1,12 @@
-const express = require("express");
-const router = express();
+const express = require("express")
+const router = express()
 
-router.use("/api/users", require("./UserRoutes"));
-router.use("/api/photos", require("./PhotoRoutes"));
+router.use("/api/users", require("./UserRoutes"))
+router.use("/api/photos", require("./PhotoRoutes"))
 
-module.exports = router;
+// test route
+router.get("/", (req, res) => {
+    res.send("API Working!");
+})
+
+module.exports = router
