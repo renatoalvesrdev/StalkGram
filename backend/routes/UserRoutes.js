19d3f73b79923c9be8da21ededee9b088c366b72
Renato Alves <renatoalvesribeiro0@gmail.com>
2023-10-16 19:34:23 -0300

    Organization the routes

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,20 +1,37 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-//Controller
-const {register, login, getCurrentUser, update, getUserById} = require("../controllers/UserController")
+// Controller
+const {
+  register,
+  getCurrentUser,
+  login,
+  update,
+  getUserById,
+} = require("../controllers/UserController");
 
-//Middlewares
-const validate = require("../middlewares/handleValidation")
-const {userCreateValidation, loginValidation, userUpdateValidation} = require("../middlewares/userValidation")
-const authGuard = require("../middlewares/authGuard")
-const { imageUpload } = require("../middlewares/imageUpload")
+// Middlewares
+const validate = require("../middlewares/handleValidations");
+const {
+  userCreateValidation,
+  loginValidation,
+  userUpdateValidation,
+} = require("../middlewares/userValidations");
+const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
 
-//Routes
+// Routes
 router.post("/register", userCreateValidation(), validate, register);
+router.get("/profile", authGuard, getCurrentUser);
 router.post("/login", loginValidation(), validate, login);
-router.get("/profile", authGuard, getCurrentUser)
-router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update)
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
 router.get("/:id", getUserById);
 
 module.exports = router;
