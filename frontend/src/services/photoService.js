23660c07a480db15257f70caf279945d0be491cf
Renato Alves <renatoalvesribeiro0@gmail.com>
2023-10-16 21:09:51 -0300

    Creating the individual photo display page

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -62,11 +62,29 @@ const updatePhoto = async(data, id, token) => {
     }
 }
 
+// Get a photo by id
+const getPhoto = async(id, token) => {
+    const config = requestConfig("GET", null, token)
+
+    try {
+        
+        const res = await fetch(api + "/photos/" + id, config)
+            .then((res) => res.json())
+            .catch((err) => err)
+
+        return res
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const photoService = {
     publishPhoto,
     getUserPhotos,
     deletePhoto,
-    updatePhoto
+    updatePhoto,
+    getPhoto
 }
 
 export default photoService
